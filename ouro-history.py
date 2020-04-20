cdf68a9a20612faaedbec6c78c897ef2982a2b96
code@agiledata.guru <code@agiledata.guru>
2020-04-19 21:39:49 -0500

    ouro-history.py is organizing Yahoo Finance data into a json format I want.

--- ouro-history.py
+++ ouro-history.py
@@ -11,6 +11,8 @@ import csv                              # for reading static data
 import yfinance as yf                   # for stock list
 import alpaca_trade_api as tradeapi     # required for trading
 import datetime                         # used for stock timestamps
+import json
+from azure.cosmos import exceptions, CosmosClient, PartitionKey
 
 # Setup Logging
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
@@ -27,17 +29,60 @@ for x in assets:
             slist.append(x.symbol)
 
 
+# Initialize the Cosmos client
+endpoint = "https://ouro.documents.azure.com:443"
+key = 'xrEm8rxq1R7wjxUZjVYP1cxX8clN4LTpOqmKUmiPc5GOOtI1GjyzjhC2VeAl0xboJqZ1Kx3la5jAB4fPxNtoCQ=='
+client = CosmosClient(endpoint, key)
+database_name = 'stockdata'
+database = client.create_database_if_not_exists(id=database_name)
+container = database.create_container_if_not_exists(
+    id="daily",
+    partition_key=PartitionKey(path="/ticker"),
+    offer_throughput=400
+)
+
+#query = "SELECT * FROM c WHERE c.lastName IN ('Wakefield', 'Andersen')"
+
+#items = list(container.query_items(
+#    query=query,
+#    enable_cross_partition_query=True
+#))
+
+#request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
+#print('Query returned {0} items. Operation consumed {1} request units'.format(len(items), request_charge))
+
 for x in slist:
     # Get daily data for last 60 days
     startdate = datetime.datetime.now() + datetime.timedelta(days=-60)
     startdate_str = startdate.strftime('%Y-%m-%d')
-    data = yf.download(x, startdate_str, interval='1d', prepost='False')
+    data = yf.download(x, startdate_str, interval='1d', prepost='False', group_by='ticker')
+    jsondata = json.loads(data.to_json(orient='index'))
+    for r in jsondata:
+        tradedate = datetime.datetime.fromtimestamp(int(r) / 1e3)
+        row = {
+            'ticker': x,
+            'tradedate': tradedate.strftime('%Y-%m-%d'),
+            'open': jsondata[r]['Open']
+            # high
+            # low
+            # close
+            # adj close
+            # volume
+        }
+        print (row)
+
+
+
+
+
+
+
+
 
     # Get intra-day data for last 24 hours
-    startdate = datetime.datetime.now() + datetime.timedelta(hours=-72)
-    startdate_str = startdate.strftime('%Y-%m-%d')
-    data = yf.download(x, startdate_str, interval='5m', prepost='False')
-    print(x)
-    print(data)
+    #startdate = datetime.datetime.now() + datetime.timedelta(hours=-72)
+    #startdate_str = startdate.strftime('%Y-%m-%d')
+    #data = yf.download(x, startdate_str, interval='5m', prepost='False')
+
 
 
