c424e3742b199623c98649d33e0f300e64316960
code@agiledata.guru <code@agiledata.guru>
2020-04-25 15:08:08 -0500

    Calcute the oscillators, interpret the signals, and write the dataset to daily_indicators

--- daily_ticalc.py
+++ daily_ticalc.py
@@ -26,8 +26,13 @@ key = os.environ.get("OURO_DOCUMENTS_KEY", "SET OURO_DOCUMENTS_KEY IN ENVIRONMEN
 client = CosmosClient(endpoint, key)
 database_name = 'stockdata'
 database = client.create_database_if_not_exists(id=database_name)
-container = database.create_container_if_not_exists(
+dhistory = database.create_container_if_not_exists(
     id="daily",
+    partition_key=PartitionKey(path="/ticker"),
+    offer_throughput=400
+)
+indicators = database.create_container_if_not_exists(
+    id="daily_indicators",
     partition_key=PartitionKey(path="/ticker"),
     offer_throughput=400
 )
@@ -36,7 +41,7 @@ logging.info ('Azure-Cosmos client initialized; connected to ' + endpoint)
 # get list of stocks in the universe
 query = "select distinct value d.ticker from daily d"
 try:
-    stocklist = list(container.query_items(
+    stocklist = list(dhistory.query_items(
         query=query,
         enable_cross_partition_query=True
     ))
@@ -52,7 +57,7 @@ for stock in stocklist:
     query = "SELECT d.id, d.ticker, d.tradedate, d.high, d.low, d.open, d.adjclose, d.volume FROM daily d where d.ticker = '" + stock + "'"
     df = pd.DataFrame()
     try:
-        data = list(container.query_items(
+        data = list(dhistory.query_items(
             query=query,
             enable_cross_partition_query=True
         ))
@@ -78,26 +83,60 @@ for stock in stocklist:
         df['AD14'] = ta.AD(df['high'], df['low'], df['adjclose'], df['volume'])
         df['OBV14'] = ta.OBV(df['adjclose'], df['volume'])
 
-## need to get the last date in daily indicators
-        for x in df:
-            row = {
-                    'id': str(id),
-                    'ticker': x,
-                    'tradedate': tradedate,
-                    'open': jsondata[r]['Open'],
-                    'high': jsondata[r]['High'],
-                    'low': jsondata[r]['Low'],
-                    'close': jsondata[r]['Close'],
-                    'adjclose': jsondata[r]['Adj Close'],
-                    'volume': jsondata[r]['Volume']
-                }
+        df['BBANDSIND'] = 'Not Interpretted'
+        df['ADX14IND'] = 'Absent'
+        df['AD14IND'] = 'Not Interpretted'
+        df['OBV14IND']= 'Not Inperpretted'
+
+        # RSI Votes
+        df['RSIVOTE'] = 0
+        df.loc[df['RSI14'] >= 70, 'RSIVOTE'] = 1
+        df.loc[df['RSI14'] <= 30, 'RSIVOTE'] = -1
+
+        # need to compare a state change from pos-to-neg or neg-to-pos for the indicator
+        df['MACDVOTE'] = 0
+        # print (df[['MACD0','MACD1','MACD2' ]])
+
+        # ADX Trend Strength
+        df.loc[df['ADX14'] >= 25, 'RSIIND'] = 'Strong'
+        df.loc[df['ADX14'] >= 50, 'RSIIND'] = 'Very Strong'
+        df.loc[df['ADX14'] >= 75, 'RSIIND'] = 'Extremely Strong'
+
+        # CCI Vote
+        df['CCIVOTE'] = 0
+        df.loc[df['CCI14'] >= 100, 'CCIVOTE'] = 1
+        df.loc[df['CCI14'] <= -100, 'CCIVOTE'] = -1
+
+        # AROON Oscillator
+        df['AROONOSC'] = df['AROONDN'] - df['AROONUP']
+        df['AROONVOTE'] = 0
+        df.loc[df['AROONOSC'] >= 25, 'AROONVOTE'] = 1    # This threshold is a guess
+        df.loc[df['AROONOSC'] <= -25, 'AROONVOTE'] = -1  # This threshold is a guess
+
+
+        # Get the max date that indicator data exists for this stock
+        # Note: 4.88 RU per 100 rows
+        query = "SELECT VALUE max(d.tradedate) from daily d where d.ticker = '" + stock + "'"
+        dt = parse('1971-01-01')
+        dt_str = dt.strftime('%Y-%m-%d')
+        try:
+            dt_list = list(indicators.query_items(
+                query=query,
+                enable_cross_partition_query=True
+            ))
+            logging.debug('Last indicator seen for ' + stock + ' on ' + dt_list[0])
+            dt = parse(dt_list[0])
+            dt_str = dt.strftime('%Y-%m-%d')
+        except:
+            logging.debug('No indicators found for ' + stock + ', creating them from scratch.')
+        rc=0
+        for x in json.loads(df.to_json(orient='records')):
+            if parse(x['tradedate']) > dt:
                 # write the data
-                # Note:  Approximately 4.2 RU
-                rucounter = rucounter + 1
                 try:
-                    #container.create_item(body=row)
-                    logging.debug('Created document for ' + x + ' on ' + tradedate)
+                    indicators.create_item(body=x)
+                    logging.debug('Created document for ' + x['ticker'] + ' on ' + x['tradedate'])
+                    rc = rc + 1
                 except:
-                    logging.error('Could not create document for ' + x + ' on ' + tradedate)
-            logging.info(
-                '(' + str(counter) + ' of ' + str(len(slist)) + ') Finished processing ' + x)
+                    logging.error('Could not create document for ' + x['ticker'] + ' on ' + x['tradedate'])
+        logging.info('Wrote ' + str(rc) + ' records for ' + stock)
