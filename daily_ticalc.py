ebe0c4748442daa7d990df75cee8500a2b2e6607
code@agiledata.guru <code@agiledata.guru>
2020-05-02 20:25:10 -0500

    Expanded the technical indicators to include 27 momentum and volume indicators.  Moved the technical indicator and interpretation calculations to ouro-lib, used the library functions to update the indicators and votes.

--- daily_ticalc.py
+++ daily_ticalc.py
@@ -14,121 +14,38 @@ from dateutil.parser import parse       # used to create date/time objects from 
 import time
 import pandas as pd
 import talib as ta
-
+import ouro_lib as ol
 
 # Setup Logging
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
 logging.info('OURO-TICALC logging enabled.')
 
 # Initialize the Cosmos client
-endpoint = os.environ.get("OURO_DOCUMENTS_ENDPOINT", "SET OURO_DOCUMENTS_ENDPOINT IN ENVIRONMENT")
-key = os.environ.get("OURO_DOCUMENTS_KEY", "SET OURO_DOCUMENTS_KEY IN ENVIRONMENT")
-client = CosmosClient(endpoint, key)
-database_name = 'stockdata'
-database = client.create_database_if_not_exists(id=database_name)
-dhistory = database.create_container_if_not_exists(
-    id="daily",
-    partition_key=PartitionKey(path="/ticker"),
-    offer_throughput=400
-)
-indicators = database.create_container_if_not_exists(
-    id="daily_indicators",
-    partition_key=PartitionKey(path="/ticker"),
-    offer_throughput=400
-)
-logging.info ('Azure-Cosmos client initialized; connected to ' + endpoint)
+dhistory = ol.cosdb('stockdata', 'daily', '/ticker')
+indicators = ol.cosdb('stockdata', 'daily_indicators', '/ticker')
 
 # get list of stocks in the universe
 query = "select distinct value d.ticker from daily d"
-try:
-    stocklist = list(dhistory.query_items(
-        query=query,
-        enable_cross_partition_query=True
-    ))
-    logging.debug('Retrieve daily data.')
-except:
-    logging.debug('No daily date available.')
+stocklist = ol.qrycosdb(dhistory, query)
 
 # process the daily stocks
 for stock in stocklist:
 
 # Get all the daily data for the past 60 days for the given stock
 # Note: 4.88 RU per 100 rows
-    query = "SELECT d.id, d.ticker, d.tradedate, d.high, d.low, d.open, d.adjclose, d.volume FROM daily d where d.ticker = '" + stock + "'"
-    df = pd.DataFrame()
-    try:
-        data = list(dhistory.query_items(
-            query=query,
-            enable_cross_partition_query=True
-        ))
-        logging.debug('Retrieve daily data for ' + stock)
-        df = pd.DataFrame(data)
-    except:
-        logging.debug('No daily date available for ' + stock)
+    query = "SELECT d.id, d.ticker, d.tradedate, d.high as h, d.low as l, d.open as o, d.adjclose as c, d.volume as v FROM daily d where d.ticker = '" + stock + "'"
+    df=pd.DataFrame(ol.qrycosdb(dhistory, query))
+
 
     if not df.empty:
-        # calculate the technical indicators if there is data to do so
-        df['RSI14'] = ta.RSI(df['adjclose'], timeperiod=14)
-        df['SMA14'] = ta.SMA(df['adjclose'], timeperiod=14)
-        df['EMA14'] = ta.EMA(df['adjclose'], timeperiod=14)
-        df['MACD0'] = ta.MACD(df['adjclose'], fastperiod=12, slowperiod=26, signalperiod=9)[0]
-        df['MACD1'] = ta.MACD(df['adjclose'], fastperiod=12, slowperiod=26, signalperiod=9)[1]
-        df['MACD2'] = ta.MACD(df['adjclose'], fastperiod=12, slowperiod=26, signalperiod=9)[2]
-        df['ADX14'] = ta.ADX(df['high'], df['low'], df['adjclose'])
-        df['CCI14'] = ta.CCI(df['high'], df['low'], df['adjclose'], timeperiod=10)
-        df['AROONUP'], df['AROONDN'] = ta.AROON(df['high'], df['low'], timeperiod=14)
-        df['BBANDS14-0'] = ta.BBANDS(df['adjclose'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)[0]
-        df['BBANDS14-1'] = ta.BBANDS(df['adjclose'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)[1]
-        df['BBANDS14-2'] = ta.BBANDS(df['adjclose'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)[2]
-        df['AD14'] = ta.AD(df['high'], df['low'], df['adjclose'], df['volume'])
-        df['OBV14'] = ta.OBV(df['adjclose'], df['volume'])
-
-        df['BBANDSIND'] = 'Not Interpretted'
-        df['ADX14IND'] = 'Absent'
-        df['AD14IND'] = 'Not Interpretted'
-        df['OBV14IND']= 'Not Inperpretted'
-
-        # RSI Votes
-        df['RSIVOTE'] = 0
-        df.loc[df['RSI14'] >= 70, 'RSIVOTE'] = 1
-        df.loc[df['RSI14'] <= 30, 'RSIVOTE'] = -1
-
-        # need to compare a state change from pos-to-neg or neg-to-pos for the indicator
-        df['MACDVOTE'] = 0
-        # print (df[['MACD0','MACD1','MACD2' ]])
-
-        # ADX Trend Strength
-        df.loc[df['ADX14'] >= 25, 'RSIIND'] = 'Strong'
-        df.loc[df['ADX14'] >= 50, 'RSIIND'] = 'Very Strong'
-        df.loc[df['ADX14'] >= 75, 'RSIIND'] = 'Extremely Strong'
-
-        # CCI Vote
-        df['CCIVOTE'] = 0
-        df.loc[df['CCI14'] >= 100, 'CCIVOTE'] = 1
-        df.loc[df['CCI14'] <= -100, 'CCIVOTE'] = -1
-
-        # AROON Oscillator
-        df['AROONOSC'] = df['AROONDN'] - df['AROONUP']
-        df['AROONVOTE'] = 0
-        df.loc[df['AROONOSC'] >= 25, 'AROONVOTE'] = 1    # This threshold is a guess
-        df.loc[df['AROONOSC'] <= -25, 'AROONVOTE'] = -1  # This threshold is a guess
-
-
         # Get the max date that indicator data exists for this stock
         # Note: 4.88 RU per 100 rows
         query = "SELECT VALUE max(d.tradedate) from daily d where d.ticker = '" + stock + "'"
         dt = parse('1971-01-01')
         dt_str = dt.strftime('%Y-%m-%d')
-        try:
-            dt_list = list(indicators.query_items(
-                query=query,
-                enable_cross_partition_query=True
-            ))
-            logging.debug('Last indicator seen for ' + stock + ' on ' + dt_list[0])
-            dt = parse(dt_list[0])
-            dt_str = dt.strftime('%Y-%m-%d')
-        except:
-            logging.debug('No indicators found for ' + stock + ', creating them from scratch.')
+        dt_list = ol.qrycosdb(indicators, query)
+        df = ol.calcind(df)
+
         rc=0
         for x in json.loads(df.to_json(orient='records')):
             if parse(x['tradedate']) > dt:
--- ouro_lib.py
+++ ouro_lib.py
@@ -50,3 +50,138 @@ def qrycosdb(ctr, query):
         logging.debug('Query did not return results:  ' + query)
         logging.debug(ex)
         return None
+
+def calcind(df):
+    # Calculate indicators and interpret them into buy or sell signals
+    # Note 1:  If the dataframe is not sorted in timeframe order, the results will be worthless
+    # Note 2:  The dataframe must have the following OHLCV attributes at a minimum:
+    #   {
+    #       "o": 100            <-- open value
+    #       "h": 150            <-- high value
+    #       "l": 90             <-- low value
+    #       "c": 110            <-- close value
+    #       "v": 3000           <-- volume value
+    #   }
+
+    if not df.empty:
+        # calculate the technical indicators if there is data to do so
+        # Ref:  https://mrjbq7.github.io/ta-lib/func_groups/momentum_indicators.html
+
+        # Momentum indicators
+        df['ADX14'] = ta.ADX(df['h'], df['l'], df['c'])
+        df['ADXR14'] = ta.ADXR(df['h'], df['l'], df['c'])
+        df['APO12'] = ta.APO(df['c'], fastperiod=12, slowperiod=26, matype=0)
+        df['AROONUP'], df['AROONDN'] = ta.AROON(df['h'], df['l'], timeperiod=14)
+        df['BOP'] = ta.BOP(df['o'], df['h'], df['l'], df['c'])
+        df['CCI14'] = ta.CCI(df['h'], df['l'], df['c'], timeperiod=14)
+        df['CMO14'] = ta.CMO(df['c'], timeperiod=14)
+        df['DX14'] = ta.DX(df['h'], df['l'], df['c'], timeperiod=14)
+        df['MACD'], df['MACDSIG'], df['MACDHIST'] = ta.MACD(df['c'], fastperiod=12, slowperiod=26, signalperiod=9)
+        df['MFI4'] = ta.MFI(df['h'], df['l'], df['c'], df['v'], timeperiod=14)
+        df['MOM10'] = ta.MOM(df['c'], timeperiod=10)
+        df['PPO12'] = ta.PPO(df['c'], fastperiod=12, slowperiod=26, matype=0)
+        df['ROC10'] = ta.MOM(df['c'], timeperiod=10)
+        df['RSI14'] = ta.RSI(df['c'], timeperiod=14)
+        df['STOCHK'], df['STOCHD'] = ta.STOCH(df['h'], df['l'], df['c'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+        df['STOCHRSIK'], df['STOCHRSID'] = ta.STOCHRSI(df['c'], timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0)
+        df['TRIX30'] = ta.TRIX(df['c'], timeperiod=30)
+        df['ULTOSC'] = ta.ULTOSC(df['h'], df['l'], df['c'], timeperiod1=7, timeperiod2=14, timeperiod3=28)
+
+        # Moving Average or Overlap functions
+        df['BBUPPER'], df['BBMID'], df['BBLOWER'] = ta.BBANDS(df['c'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
+        df['EMA14'] = ta.EMA(df['c'], timeperiod=14)
+        df['SMA14'] = ta.SMA(df['c'], timeperiod=14)
+
+        # Volume Indicators
+        df['AD'] = ta.AD(df['h'], df['l'], df['c'], df['v'])
+        df['ADOSC'] = ta.ADOSC(df['h'], df['l'], df['c'], df['v'], fastperiod=3, slowperiod=10)
+        df['OBV'] = ta.OBV(df['c'], df['v'])
+
+        # ADX Trend Strength
+        df['ADXTREND'] = 'Weak'
+        df.loc[df['ADX14'] >= 25, 'ADXTREND'] = 'Changing'
+        df.loc[df['ADX14'] >= 50, 'ADXTREND'] = 'Strong'
+        df.loc[df['ADX14'] >= 75, 'ADXTREND'] = 'Very Strong'
+
+        # ADXR Trend Strength
+        df['ADXRTREND'] = 'Weak'
+        df.loc[df['ADXR14'] >= 25, 'ADXRTREND'] = 'Changing'
+        df.loc[df['ADXR14'] >= 50, 'ADXRTREND'] = 'Strong'
+        df.loc[df['ADXR14'] >= 75, 'ADXRTREND'] = 'Very Strong'
+
+        # AROON Oscillator
+        df['AROONOSC'] = df['AROONDN'] - df['AROONUP']
+        df['AROONVOTE'] = 0
+        df.loc[df['AROONOSC'] >= 25, 'AROONVOTE'] = 1    # This threshold is a guess
+        df.loc[df['AROONOSC'] <= -25, 'AROONVOTE'] = -1  # This threshold is a guess
+
+        # BOP Signal
+        df['BOPVOTE'] = 0
+        df.loc[df['BOP'] >0, 'BOPVOTE'] = 1
+        df.loc[df['BOP'] <0, 'BOPVOTE'] = -1
+
+        # CCI Vote
+        df['CCIVOTE'] = 0
+        df.loc[df['CCI14'] >= 100, 'CCIVOTE'] = 1
+        df.loc[df['CCI14'] <= -100, 'CCIVOTE'] = -1
+
+        # CMO Votes
+        df['CMOVOTE'] = 0
+        df.loc[df['CMO14'] < -50, 'CMOVOTE'] = 1
+        df.loc[df['CMO14'] > 50, 'CMOVOTE'] = -1
+
+        # MACD Vote; based on when the histogram crosses the zero line
+        df['MACDVOTE'] = 0
+        df.loc[(df['MACDHIST'] > 0) & (df['MACDHIST'].shift(periods=1) <= 0), 'MACDVOTE'] = 1
+        df.loc[(df['MACDHIST'] <= 0) & (df['MACDHIST'].shift(periods=1) > 0), 'MACDVOTE'] = -1
+        df.loc[df['MACDHIST'] == 0, 'CMOVOTE'] = 0
+
+        # MFI Votes
+        # Skipping interpretting MFI because it correlates to the direction of price
+
+        # MOM Votes
+        # Skipping basic momentum because it's not a good signal for buy or sell
+
+        # PPO Votes; cousin of MACD
+        df['PPOVOTE'] = 0
+        df.loc[df['PPO12'] >= 0, 'RSIVOTE'] = 1
+        df.loc[df['PPO12'] <= 0, 'RSIVOTE'] = -1
+
+        # ROC Votes
+        # Not using ROC because it's prone to whipsaws near the 0 line; and, this isn't used to trade
+
+        # RSI Votes
+        df['RSIVOTE'] = 0
+        df.loc[df['RSI14'] >= 70, 'RSIVOTE'] = 1
+        df.loc[df['RSI14'] <= 30, 'RSIVOTE'] = -1
+
+        # STOCH Votes
+        df['STOCHVOTE'] = 0
+        df.loc[(df['STOCHK'] >= 80) & (df['STOCHD'] >= 80), 'STOCHVOTE'] = -1
+        df.loc[(df['STOCHK'] <= 20) & (df['STOCHD'] <= 20), 'STOCHVOTE'] = 1
+
+        # STOCHRSI Votes
+        df['STOCHRSIVOTE'] = 0
+        df.loc[(df['STOCHRSIK'] >= 80) & (df['STOCHRSID'] >= 80), 'STOCHRSIVOTE'] = -1
+        df.loc[(df['STOCHRSIK'] <= 20) & (df['STOCHRSID'] <= 20), 'STOCHRSIVOTE'] = 1
+
+        # TRIX Votes
+        df['TRIXVOTE'] = 0
+        df.loc[df['TRIX30'] > 0, 'TRIXVOTE'] = 1
+        df.loc[df['TRIX30'] < 0, 'TRIXVOTE'] = -1
+
+        # ULTOSC Votes
+        # I'm skipping this oscillator because the buy/sell conditions are three-pronged and not clear
+
+        # ADOSC Votes
+        df['ADOSCVOTE'] = 0
+        df.loc[df['ADOSC'] > 0, 'ADOSCVOTE'] = 1
+        df.loc[df['ADOSC'] < 0, 'ADOSCVOTE'] = -1
+
+        return df
+
+
+
+
+
+
