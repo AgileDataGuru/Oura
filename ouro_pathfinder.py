f432b71826567fcd77dfa7793ab7ef1d7c5353a8
code@agiledata.guru <code@agiledata.guru>
2020-05-02 23:32:30 -0500

    Renamed the HLOCV columns to match the common function requirements.

--- ouro_pathfinder.py
+++ ouro_pathfinder.py
@@ -43,60 +43,15 @@ df = {}
 # Convert barset to usable dataframe
 for stock in barset.keys():
     bars = barset[stock]
-    data = {'adjclose': [bar.c for bar in bars],
-            'high': [bar.h for bar in bars],
-            'low': [bar.l for bar in bars],
-            'open': [bar.o for bar in bars],
-            'time': [bar.t for bar in bars],
-            'volume': [bar.v for bar in bars]}
-    df[stock] = pd.DataFrame(data)
+    data = {'t': [bar.t for bar in bars],
+            'h': [bar.h for bar in bars],
+            'l': [bar.l for bar in bars],
+            'o': [bar.o for bar in bars],
+            'c': [bar.c for bar in bars],
+            'v': [bar.v for bar in bars]}
+    df[stock] = ol.calcind(pd.DataFrame(data))
+    print(df[stock])
 
-    if not df[stock].empty:
-        # calculate the technical indicators if there is data to do so
-        df[stock]['RSI14'] = ta.RSI(df[stock]['adjclose'], timeperiod=14)
-        df[stock]['SMA14'] = ta.SMA(df[stock]['adjclose'], timeperiod=14)
-        df[stock]['EMA14'] = ta.EMA(df[stock]['adjclose'], timeperiod=14)
-        df[stock]['MACD0'] = ta.MACD(df[stock]['adjclose'], fastperiod=12, slowperiod=26, signalperiod=9)[0]
-        df[stock]['MACD1'] = ta.MACD(df[stock]['adjclose'], fastperiod=12, slowperiod=26, signalperiod=9)[1]
-        df[stock]['MACD2'] = ta.MACD(df[stock]['adjclose'], fastperiod=12, slowperiod=26, signalperiod=9)[2]
-        df[stock]['ADX14'] = ta.ADX(df[stock]['high'], df[stock]['low'], df[stock]['adjclose'])
-        df[stock]['CCI14'] = ta.CCI(df[stock]['high'], df[stock]['low'], df[stock]['adjclose'], timeperiod=10)
-        df[stock]['AROONUP'], df[stock]['AROONDN'] = ta.AROON(df[stock]['high'], df[stock]['low'], timeperiod=14)
-        df[stock]['BBANDS14-0'] = ta.BBANDS(df[stock]['adjclose'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)[0]
-        df[stock]['BBANDS14-1'] = ta.BBANDS(df[stock]['adjclose'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)[1]
-        df[stock]['BBANDS14-2'] = ta.BBANDS(df[stock]['adjclose'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)[2]
-        df[stock]['AD14'] = ta.AD(df[stock]['high'], df[stock]['low'], df[stock]['adjclose'], df[stock]['volume'])
-        df[stock]['OBV14'] = ta.OBV(df[stock]['adjclose'], df[stock]['volume'])
-
-        df[stock]['BBANDSIND'] = 'Not Interpretted'
-        df[stock]['ADX14IND'] = 'Absent'
-        df[stock]['AD14IND'] = 'Not Interpretted'
-        df[stock]['OBV14IND']= 'Not Inperpretted'
-
-        # RSI Votes
-        df[stock]['RSIVOTE'] = 0
-        df[stock].loc[df[stock]['RSI14'] >= 70, 'RSIVOTE'] = 1
-        df[stock].loc[df[stock]['RSI14'] <= 30, 'RSIVOTE'] = -1
-
-        # need to compare a state change from pos-to-neg or neg-to-pos for the indicator
-        df[stock]['MACDVOTE'] = 0
-        # print (df[['MACD0','MACD1','MACD2' ]])
-
-        # ADX Trend Strength
-        df[stock].loc[df[stock]['ADX14'] >= 25, 'RSIIND'] = 'Strong'
-        df[stock].loc[df[stock]['ADX14'] >= 50, 'RSIIND'] = 'Very Strong'
-        df[stock].loc[df[stock]['ADX14'] >= 75, 'RSIIND'] = 'Extremely Strong'
-
-        # CCI Vote
-        df[stock]['CCIVOTE'] = 0
-        df[stock].loc[df[stock]['CCI14'] >= 100, 'CCIVOTE'] = 1
-        df[stock].loc[df[stock]['CCI14'] <= -100, 'CCIVOTE'] = -1
-
-        # AROON Oscillator
-        df[stock]['AROONOSC'] = df[stock]['AROONDN'] - df[stock]['AROONUP']
-        df[stock]['AROONVOTE'] = 0
-        df[stock].loc[df[stock]['AROONOSC'] >= 25, 'AROONVOTE'] = 1    # This threshold is a guess
-        df[stock].loc[df[stock]['AROONOSC'] <= -25, 'AROONVOTE'] = -1  # This threshold is a guess
 
 #
 #
