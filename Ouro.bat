bd20ba183b61272a4c21b7f64f0b4ea6995849b5
Agile Data Guru <code@agiledata.guru>
2020-06-06 15:53:44 -0500

    Added ouro_accountant to the daily batch file; added code to move the log files to a folder in the quorum

--- Ouro.bat
+++ Ouro.bat
@@ -1,2 +1,27 @@
-py D:\OneDrive\Dev\Python\Oura\daily_history.py
-py D:\OneDrive\Dev\Python\Oura\daily_ticalc.py
+@ECHO OFF
+
+REM OURO:  Automates routine daily tasks
+REM Written by Dave Andrus on June 6, 2020
+REM Copyright 2020 Agile Data Guru
+REM https://github.com/AgileDataGuru/Ouro
+
+REM Install path is %OURO_INSTALL%
+REM Quorum path is %OURO_QUORUM%
+
+REM Get daily history
+py %OURO_INSTALL%\daily_history.py
+
+REM Calculate daily technical indicators
+py %OURO_INSTALL%\daily_ticalc.py
+
+REM Set the archive directory
+SET ad=%OURO_QUORUM%\%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+
+REM Make the archive folder
+mkdir ad
+
+REM Move the files
+robocopy %OURO_QUORUM% %ad% /MOV
+
+REM Tally daily totals
+py %OURO_INSTALL%\ouro_accountant.py
